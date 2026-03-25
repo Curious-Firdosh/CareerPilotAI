34ed27597d6d85d0128e51c8979af3f8f8a2fc2b
Curious-Firdosh <thefirdosh@gmail.com>
2026-03-25 20:19:05 +0530

    Connected frontend And Backend and connected Login Signup frontend with Backend

--- Backend/src/utils/ENV.js
+++ Backend/src/utils/ENV.js
@@ -4,5 +4,9 @@ dotenv.config()
 
 export const ENV = {
     Mongodb_Url: process.env.MONGODB_URI|| "",
-    JWT_Secret : process.env.JWT_SECRET
+    JWT_Secret : process.env.JWT_SECRET,
+    Frontend_URL : process.env.Frontend_URL || "http://localhost:5173",
+    API_KEY : process.env.API_KEY || ""
 }
+
+
